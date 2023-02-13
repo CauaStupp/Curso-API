9a027753c42b97d6254e5e364c1cbe8a17f6951c
Cauã Vinicius Chaves Stupp <caua80190@gmail.com>
2023-02-13 15:34:58 -0300

    update

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const cors = require('cors');
+
+const CarroController = require('./controllers/CarroController');
+
+
+
+router.get('/carros', CarroController.buscarTodos);
+router.get('/carro/:codigo', CarroController.buscarUm);
+router.post('/carro', CarroController.inserir);
+router.put('/carro/:codigo', CarroController.alterar);
+router.delete('/carro/:codigo', CarroController.excluir);
+
+module.exports = router;
